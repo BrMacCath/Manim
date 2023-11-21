34efb114e14037664d30b8cb7d8c2834e0da5c0a
BrMacCath <brian.collery@colostate.edu>
2023-11-20 18:20:30 -1100

    Major fixes in Eigenvalue scene.

--- Math 618/ellipse_test.py
+++ Math 618/ellipse_test.py
@@ -26,32 +26,81 @@ class EigenVectors(Scene):
     def construct(self):
         # Here we will first talk about Eigenvectors
         self.wait(1)
-        self.play(TitleCards(Tex("Eigen values").scale(3).shift(2*UP)),run_time=2)
+        self.play(TitleCards(Tex("EigenVectors").scale(3).shift(2*UP)),run_time=2)
 
         # Properties of the first ellipse
         height_1 = ValueTracker(1)
         width_1 = ValueTracker(1)
-        c_1x =ValueTracker(-4)
-        c_1y =ValueTracker(-1)
+        c_1x = ValueTracker(-4)
+        c_1y = ValueTracker(-1)
+        c_2x = ValueTracker(4)
+        c_2y= ValueTracker(-1)
         angle_1 = ValueTracker(0)
-        ellipse_1 =  always_redraw(lambda: Ellipse(width=1.5*width_1.get_value(), height=1.5*height_1.get_value()).rotate(angle=angle_1.get_value()).shift(c_1x.get_value()*RIGHT+ c_1y.get_value()*UP))
+        ellipse_1 =  always_redraw(lambda: Ellipse(width=1.5, height=1.5).rotate(angle=angle_1.get_value()).shift(c_1x.get_value()*RIGHT+ c_1y.get_value()*UP))
+        ellipse_2 =  always_redraw(lambda: Ellipse(width=1.5*width_1.get_value(), height=1.5*height_1.get_value()).rotate(angle=angle_1.get_value()).shift(c_2x.get_value()*RIGHT+ c_2y.get_value()*UP))
         ax_1 = always_redraw(lambda: Axes(
             x_length=4,
             y_length=4,
             axis_config={"include_ticks": False}
         ).shift(c_1x.get_value()*RIGHT+ c_1y.get_value()*UP))
+        ax_2 = always_redraw(lambda: Axes(
+            x_length=4,
+            y_length=4,
+            axis_config={"include_ticks": False}
+        ).shift(c_2x.get_value()*RIGHT+ c_2y.get_value()*UP))
         # This is going to represent the eigenvector. It is needs to be defined in terms of the the center of the ellipse.
-        vect_1 = always_redraw(lambda: Line(start=ellipse_1.get_center(),end=ellipse_1.get_center()+[1.5*height_1.get_value()*np.cos(PI/2 + angle_1.get_value())/2,1.5*height_1.get_value()*np.sin(PI/2 + angle_1.get_value())/2,0]).add_tip())
-
+        vect_1 = always_redraw(lambda: Line(start=ellipse_1.get_center(),end=ellipse_1.get_center()+[1.5*np.cos(PI/2 + angle_1.get_value())/2,1.5*np.sin(PI/2 + angle_1.get_value())/2,0]).add_tip())
+        vect_2 = always_redraw(lambda:Line(start=ellipse_1.get_center(),end=ellipse_1.get_center()+[1.5*np.cos( angle_1.get_value())/2,1.5*np.sin( angle_1.get_value())/2,0]).add_tip())
         # This will be the Matrices that talk about the action
         lamMatrix = always_redraw(lambda:DecimalMatrix([[height_1.get_value(),0],[0,width_1.get_value()]]).shift(2*UP))
         inputMat = DecimalMatrix([[1],[2]]).next_to(lamMatrix,direction=RIGHT)
         outputMat = DecimalMatrix([[1],[2]]).next_to(lamMatrix,direction=LEFT)
         columns = lamMatrix.get_columns()
-        self.add(ellipse_1,vect_1,lamMatrix.get_brackets(),inputMat,outputMat,ax_1)
-        self.play(angle_1.animate.set_value(PI/2))
+
+        TempMatrix = MathTex(r"\begin{bmatrix}"
+                             r"a & b\\"
+                             r"c & d"
+                             r"\end{bmatrix}").shift(2*UP)
+        self.play(Create(TempMatrix))
+
+        self.wait(3)
+        
+        self.play(Create(ax_1),run_time=2)
+        self.play(GrowFromCenter(ellipse_1),run_time=2)
+        self.play(Create(vect_1),Create(vect_2),run_time=2)
+        self.play(Create(ax_2), run_time=3)
+        self.play(GrowFromCenter(ellipse_2),run_time=2)
+        
+        self.play(angle_1.animate.set_value(PI/4))
+        out_1 = vect_1.copy()
+        out_2 = vect_2.copy()
+        self.add(out_1)
+        self.add(out_2)
+        
+
+        out_1.clear_updaters()
+        out_2.clear_updaters()
+        self.play(out_1.animate.shift(  (c_2x.get_value()-c_1x.get_value())*RIGHT  ),out_2.animate.shift(  (c_2x.get_value()-c_1x.get_value())*RIGHT  ))
+
+        outvec_1 =always_redraw(lambda: Line(ellipse_2.get_center(),end=ellipse_2.get_center()+[1.5*height_1.get_value()*np.cos(PI/2 + angle_1.get_value())/2,1.5*height_1.get_value()*np.sin(PI/2 + angle_1.get_value())/2,0]).add_tip())
+        outvec_2 =always_redraw(lambda: Line(ellipse_2.get_center(),end=ellipse_2.get_center()+[1.5*width_1.get_value()*np.cos( angle_1.get_value())/2,1.5*width_1.get_value()*np.sin(PI/2 + angle_1.get_value())/2,0]).add_tip())
+        self.remove(out_1,out_2 )
+        self.add(outvec_1,outvec_2)
+        self.play(FadeOut(TempMatrix))
+
+
+        # Inputs and Outpus
+        self.add(inputMat,outputMat)
+
+        self.play(Create(lamMatrix.get_brackets()))
+        
         self.wait(1)
-        self.play(FadeIn(columns[0]))
+        self.play(FadeIn(lamMatrix))
+        self.wait(1)
+
+        self.play(height_1.animate.set_value(2))
+        self.wait(1)
+        self.play(width_1.animate.set_value(0.5))
         self.wait(1)
 
 
@@ -60,15 +109,19 @@ class SingularValues(Scene):
     # an easy example
     def construct(self):
         ## First a scene where they are introduced
+        self.play(TitleCards(Tex("Singular Value Decomposition").scale(3).shift(2*UP)),run_time=2)
 
-
+        self.wait(1)
         ## Then a scene where we can go through an easy example involving
         ## rotations and stretches. 
-        pass
+
 
 
 class PCA(Scene):
     def construct(self):
+
+        self.play(TitleCards(Tex("Principal component Analysis").scale(3).shift(2*UP)),run_time=2)
+
         # Try to create a visual reason to look at PCA. 
         # Go through an example with 3 points that 
         # highlight the reason for PCA. Show that 
